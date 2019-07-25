85fd0bb40f53670f7bc8b314c46b6724a652b613
NINOMIYA, Tetsuro <ninottsu@gmail.com>
2019-07-25 15:28:06 +0900

    add State.py: this module calculates elapsed time

---
+++ navigation/main.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# Solar-boat Project 2019
+# created on: 2019/07/25
+# Author: Tetsuro Ninomiya
+#
+
+'''
+NOW I'M MAKING!
+WAIT FOR A WHILE!
+'''
+
+def main():
+    print('Hello, world!')
+
+if __name__ == "__main__":
+    main()
